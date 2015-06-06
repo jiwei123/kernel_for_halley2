596db95bd5efcc65966d45bd922673f1a15295ad
hhyang <hhyang_sz@ingenic.cn>
2015-06-06 18:47:56 +0800

    watch:add new board aw808

--- arch/mips/xburst/soc-m200/chip-m200/watch/aw808/pmu.h
+++ arch/mips/xburst/soc-m200/chip-m200/watch/aw808/pmu.h
@@ -2,47 +2,47 @@
 #define __PMU_H__
 #ifdef CONFIG_REGULATOR_RICOH619
 
-#define PMU_I2C_BUSNUM  1
+#define PMU_I2C_BUSNUM  0
 /* ****************************PMU DC/LDO NAME******************************* */
 #define DC1_NAME "cpu_core"
 #define DC2_NAME "cpu_vmema"
 #define DC3_NAME "cpu_mem12"
 #define DC4_NAME "cpu_vddio"
 #define DC5_NAME "vcc_gps_1v8"
-#define LDO1_NAME "wifi_vddio_1v8"
+#define LDO1_NAME "lcd_blk_vcc"
 #define LDO2_NAME "emmc_vcc"
-#define LDO3_NAME "vcc_tp_3v0"
-#define LDO4_NAME "cpu_vddio2"
+#define LDO3_NAME "mpu9255"
+#define LDO4_NAME "lcd_1v8"
 #define LDO5_NAME "cpu_2v5"
-#define LDO6_NAME "v33"
-#define LDO7_NAME "vcc_sensor1v8"
-#define LDO8_NAME "vcc_sensor3v3"
-#define LDO9_NAME "lcd_1v8"
-#define LDO10_NAME "lcd_3v0"
+#define LDO6_NAME "lcd_3v0"
+#define LDO7_NAME "vcc_sensor3v3"
+#define LDO8_NAME "vcc_sensor1v8"
+#define LDO9_NAME "vcc_tp_1v8"
+#define LDO10_NAME "vcc_tp_3v0"
 #define LDORTC1_NAME "rtc_1v8"
 #define LDORTC2_NAME "rtc_1v1"
 /* ****************************PMU DC/LDO NAME END*************************** */
 
 /* ****************************PMU DC/LDO DEFAULT V************************** */
-#define DC1_INIT_UV     1100
+#define DC1_INIT_UV     1175
 #define DC2_INIT_UV     1200
-#define DC3_INIT_UV     1200
+#define DC3_INIT_UV     3300
 #ifdef CONFIG_JZ_EPD_V12
-#define DC4_INIT_UV     3300
+#define DC4_INIT_UV     1800
 #else
 #define DC4_INIT_UV     1800
 #endif
-#define DC5_INIT_UV     1800
-#define LDO1_INIT_UV    1800
+#define DC5_INIT_UV     3300
+#define LDO1_INIT_UV    3400
 #define LDO2_INIT_UV    3300
-#define LDO3_INIT_UV    3000
+#define LDO3_INIT_UV    2500
 #define LDO4_INIT_UV    1800
 #define LDO5_INIT_UV    2500
-#define LDO6_INIT_UV    3300
-#define LDO7_INIT_UV    1800
-#define LDO8_INIT_UV    3300
+#define LDO6_INIT_UV    2800
+#define LDO7_INIT_UV    3300
+#define LDO8_INIT_UV    1800
 #define LDO9_INIT_UV    1800
-#define LDO10_INIT_UV   3000
+#define LDO10_INIT_UV   2800
 #define LDORTC1_INIT_UV 1800
 #define LDORTC2_INIT_UV 1100
 /* ****************************PMU DC/LDO DEFAULT V END********************** */
@@ -76,7 +76,7 @@
 #define DC3_ALWAYS_ON     0
 #define DC4_ALWAYS_ON     1
 #define DC5_ALWAYS_ON     0
-#define LDO1_ALWAYS_ON    0
+#define LDO1_ALWAYS_ON    1
 #define LDO2_ALWAYS_ON    1
 #define LDO3_ALWAYS_ON    0
 #define LDO4_ALWAYS_ON    1
@@ -96,7 +96,7 @@
 #define DC3_BOOT_ON     0
 #define DC4_BOOT_ON     1
 #define DC5_BOOT_ON     0
-#define LDO1_BOOT_ON    0
+#define LDO1_BOOT_ON    1
 #define LDO2_BOOT_ON    1
 #define LDO3_BOOT_ON    0
 #define LDO4_BOOT_ON    1
