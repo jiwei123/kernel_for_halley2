422b3f7d20a217a426cdc34ea060651abece647d
HuangLihong <lihong.huang@ingenic.com>
2016-04-11 13:12:44 +0800

    uart: for pdma mode, move stack to tcsm bank0; move data section to tcsm bank1

--- drivers/dma/jzdma/common_firmware/include/tcsm.h
+++ drivers/dma/jzdma/common_firmware/include/tcsm.h
@@ -10,10 +10,10 @@ struct mailbox_pend_addr_s {
 };
 
 // IRQ Pending Flag Space : up to 64 irq
-#define MAILBOX_PEND_ADDR       0xF4007000
+#define MAILBOX_PEND_ADDR       0xF4001000
 
 // UART Allocate Space
-#define TCSM_UART_BASE_ADDR     0xF4007100
+#define TCSM_UART_BASE_ADDR     0xF4001100
 #define TCSM_UART_TBUF_WP       (TCSM_UART_BASE_ADDR + 0x4)
 #define TCSM_UART_TBUF_RP       (TCSM_UART_TBUF_WP + 0x4)
 #define TCSM_UART_RBUF_WP       (TCSM_UART_TBUF_RP + 0x4)
@@ -21,10 +21,10 @@ struct mailbox_pend_addr_s {
 #define TCSM_UART_DEVICE_NUM    (TCSM_UART_RBUF_RP + 0x4)
 #define TCSM_UART_DEBUG         (TCSM_UART_DEVICE_NUM + 0x4)
 
-#define TCSM_UART_TBUF_ADDR     (0xF4007200)
-#define TCSM_UART_TBUF_LEN      0x800
-#define TCSM_UART_RBUF_ADDR     0xF4006000
-#define TCSM_UART_RBUF_LEN      0x1000
+#define TCSM_UART_TBUF_ADDR     (0xF4001400)
+#define TCSM_UART_TBUF_LEN      0x400
+#define TCSM_UART_RBUF_ADDR     (TCSM_UART_TBUF_ADDR + TCSM_UART_TBUF_LEN)
+#define TCSM_UART_RBUF_LEN      0x800
 
 #define TCSM_UART_NEED_READ     (0x1 << 0)
 #define TCSM_UART_NEED_WRITE    (0x1 << 1)
